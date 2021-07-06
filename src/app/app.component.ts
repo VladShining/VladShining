6858f6fa52c543798abc2ba01a8cc117602f701e
vlad shining <vladshining@gmail.com>
2021-07-06 14:49:50 +0300

    generate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import '../environments/firebase';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,30 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { ViewComponent } from './view/view.component';
+import { ResumeComponent } from './resume/resume.component';
+import { SelectorComponent } from './selector/selector.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfilComponent } from './profil/profil.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkComponent } from './work/work.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    SelectorComponent,
+    ViewComponent,
+    HeadComponent,
+    FooterComponent,
+    ProfilComponent,
+    EducationComponent,
+    SkillsComponent,
+    WorkComponent,
+    HobbiesComponent,
   ],
   imports: [
     BrowserModule,
@@ -17,10 +38,10 @@ import { environment } from '../environments/environment';
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
